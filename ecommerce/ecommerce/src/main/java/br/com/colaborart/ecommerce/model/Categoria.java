bc08dad5c163fc09f17d362f522cd78859b2ca61
Vitor Borela <borel4.lpv@gmail.com>
2020-12-10 11:01:06 -0300

    segundos comentarios 10.12.2020.

--- ecommerce/ecommerce/src/main/java/br/com/colaborart/ecommerce/model/Categoria.java
+++ ecommerce/ecommerce/src/main/java/br/com/colaborart/ecommerce/model/Categoria.java
@@ -12,18 +12,24 @@ import javax.validation.constraints.Size;
 @Table(name = "tb_categoria")
 public class Categoria {
 
+	
+	// linha de numero de identificacao(idCategoria) dentro de uma categoria(ex bones , camisetas , calca , etc ) .  
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long idCategoria;
 
+	// linha de String para tipo de Produto -- adequado para organizacao do tipo de produto aprensentado na landing page.
 	@NotBlank
-	@Size(min = 3, max = 50) // primeiroo comentario do codigo que nao faz sentindo ainda
+	@Size(min = 3, max = 50) 
 	private String tipoProduto;
 
+	// String para descricao da categoria do produto - material de composicao , linha(estilo/colecao) .
 	@NotBlank
 	@Size(min = 5, max = 200)
 	private String descricaoCategoria;
 
+	
+	 // getters & setters dos atributos do model.
 	public long getIdCategoria() {
 		return idCategoria;
 	}
