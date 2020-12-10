cb3e149257c0d62df566d9dc0f10786935ef6932
Bruno Augusto Falango <brunofalango1997@gmail.com>
2020-12-10 10:31:01 -0300

    task 4 - model categoria

---
+++ ecommerce/ecommerce/src/main/java/br/com/colaborart/ecommerce/model/Categoria.java
@@ -0,0 +1,51 @@
+package br.com.colaborart.ecommerce.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idCategoria;
+
+	@NotBlank
+	@Size(min = 3, max = 50)
+	private String tipoProduto;
+
+	@NotBlank
+	@Size(min = 5, max = 200)
+	private String descricaoCategoria;
+
+	public long getIdCategoria() {
+		return idCategoria;
+	}
+
+	public void setIdCategoria(long idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+
+	public String getTipoProduto() {
+		return tipoProduto;
+	}
+
+	public void setTipoProduto(String tipoProduto) {
+		this.tipoProduto = tipoProduto;
+	}
+
+	public String getDescricaoCategoria() {
+		return descricaoCategoria;
+	}
+
+	public void setDescricaoCategoria(String descricaoCategoria) {
+		this.descricaoCategoria = descricaoCategoria;
+	}
+
+}
